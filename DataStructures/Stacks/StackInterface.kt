f1c7175a5b09606b4fbb7ce5ae2d5eb937c9c3c7
CoRoGi <cory.gipson@gmail.com>
2023-08-06 16:42:17 -0400

    Stack Implementation

---
+++ DataStructures/Stacks/StackInterface.kt
@@ -0,0 +1,15 @@
+package Stacks
+
+interface StackInterface<T: Any> {
+
+    val storage: ArrayList<T>
+
+    val size: Int
+
+    fun peek(): T?
+
+    fun push(item: T)
+
+    fun pop(): T?
+
+  }
