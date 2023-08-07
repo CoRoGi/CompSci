248a59da212da04f91d8d661217d786e63ffa942
CoRoGi <cory.gipson@gmail.com>
2023-08-06 23:14:49 -0400

    Stack Implementation

---
+++ DataStructures/Stacks/Stack.kt
@@ -0,0 +1,35 @@
+package Stacks
+
+import Stacks.StackInterface
+
+class Stack<T: Any>: StackInterface<T> {
+
+    override val storage = ArrayList<T>()
+
+    override var size = 0
+
+    override fun peek(): T? {
+        if (storage.isEmpty()) {
+            return null
+          }
+        else {
+            return storage[size - 1]
+          }
+      }
+
+    override fun push(item: T) {
+      storage.add(item)
+      size++
+      }
+
+    override fun pop(): T? {
+        if (storage.isEmpty()) {
+            return null
+          }
+        else {
+            val poppedItem = storage.removeLastOrNull()
+            size--
+            return poppedItem
+          }
+      }
+  }
